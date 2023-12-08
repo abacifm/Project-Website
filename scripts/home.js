45b877eeaabf481d90bd604f73610e56e2e15c90
aak2054 <anamo241@outlook.com>
2023-12-08 18:31:22 -0500

    This commit is to setup the website

---
+++ scripts/home.js
@@ -0,0 +1,6 @@
+// this will be the image slider on the home page section
+// this will be the image slider on the home page section
+import React from 'react';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+import { SliderBox } from "react-native-image-slider-box";
+
