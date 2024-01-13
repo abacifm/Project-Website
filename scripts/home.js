a5aa554d8a748805238566716e1aeea5536b2616
aak2054 <anamo241@outlook.com>
2024-01-12 19:30:09 -0500

    updated the image slider script

--- scripts/home.js
+++ scripts/home.js
@@ -1,6 +1,29 @@
 // this will be the image slider on the home page section
-// this will be the image slider on the home page section
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
-import { SliderBox } from "react-native-image-slider-box";
+var imgs = document.querySelectorAll('.slider img');
+var dots = document.querySelectorAll('.dot');
+var currentImg = 0; // index of the first image 
+var timer; // initialize timer variable
+const interval = 3000; // duration(speed) of the slider
 
+function changeSlide(n) {
+  for (var i = 0; i < imgs.length; i++) { // reset
+    imgs[i].style.opacity = 0;
+    dots[i].classList.remove('active');
+  }
+
+  currentImg = (n !== undefined) ? n : (currentImg + 1) % imgs.length; // update the index number
+
+  imgs[currentImg].style.opacity = 1;
+  dots[currentImg].classList.add('active');
+}
+
+function startSlider() {
+  timer = setInterval(changeSlide, interval);
+}
+
+function stopSlider() {
+  clearInterval(timer);
+}
+
+changeSlide(); // Start the slideshow immediately
+startSlider(); // Set interval for automatic changes
